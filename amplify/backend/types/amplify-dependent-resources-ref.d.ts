3cd70ffa8e22cc95ce8dd9aef4d0083180fd1a9a
vvillar-eoschools <vvillar@eoschools.org>
2022-10-27 22:05:00 -0300

    authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifycypress6ccacc05": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
